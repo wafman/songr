c4aa5cebc63306769c020c01395ebd291e3d1e1c
Will Fritts <waf515@gmail.com>
2019-06-12 08:39:39 -0700

    initial commit from Spring init

---
+++ src/main/java/com/fritts/java401d4/spring/Lab/songr/SongrApplication.java
@@ -0,0 +1,13 @@
+package com.fritts.java401d4.spring.Lab.songr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fritts/java401d4/spring/Lab/songr/SongrApplicationTests.java
@@ -0,0 +1,16 @@
+package com.fritts.java401d4.spring.Lab.songr;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SongrApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
