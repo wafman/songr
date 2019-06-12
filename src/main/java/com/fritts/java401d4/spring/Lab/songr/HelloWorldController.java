16393c87b75f920ee0f1ef84c24b0a14bb4967c3
Will Fritts <waf515@gmail.com>
2019-06-12 11:41:36 -0700

    done with feature tasks

--- src/main/java/com/fritts/java401d4/spring/Lab/songr/HelloWorldController.java
+++ src/main/java/com/fritts/java401d4/spring/Lab/songr/HelloWorldController.java
@@ -2,6 +2,10 @@ package com.fritts.java401d4.spring.Lab.songr;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 
 public class HelloWorldController {
 
@@ -15,8 +19,19 @@ public class HelloWorldController {
         return word.toUpperCase();
     }
 
-    @GetMapping("/reverse?sentence={sentence}")
-    public String getReverseSentence(@PathVariable String sentence){
+    @GetMapping("/reverse")
+    public String getReverseSentence(@RequestParam(required = false) String sentence){
+        String[] words = sentence.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for(int i = words.length -1; i >= 0; i--){
+            sb.append(words[i]);
+            sb.append(" ");
+        }
+        return sb.toString();
+    }
+
+    @GetMapping("/reverseER")
+    public String getCoolReverseSentence(@RequestParam(required = false) String sentence){
         StringBuilder sb = new StringBuilder();
         sb.append(sentence);
         sb = sb.reverse();
