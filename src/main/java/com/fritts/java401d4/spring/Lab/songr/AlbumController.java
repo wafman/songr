36590256d4c805d249b433bbfe06a76ac5cba78d
Will Fritts <waf515@gmail.com>
2019-06-13 12:24:12 -0700

    feature 3 done: add new album

--- src/main/java/com/fritts/java401d4/spring/Lab/songr/AlbumController.java
+++ src/main/java/com/fritts/java401d4/spring/Lab/songr/AlbumController.java
@@ -4,6 +4,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.view.RedirectView;
 
 @Controller
 public class AlbumController {
@@ -11,11 +14,23 @@ public class AlbumController {
     @Autowired
     AlbumRespository albumRespository;
 
-    @GetMapping("/album")
+    @GetMapping("/albums")
     public String getAllALbums(Model model){
         Iterable<Album> albums = albumRespository.findAll();
         model.addAttribute("albums", albums);
-        return "album";
+        return "albums";
+    }
+
+    @GetMapping("/albums/new")
+    public String getAddAlbumForm() {
+        return "AlbumForm";
+    }
+
+    @PostMapping("/albums")
+    public RedirectView addGreeting(@RequestParam String title, @RequestParam String artist, @RequestParam int songCount, @RequestParam int length, @RequestParam String imageURL) {
+        Album album = new Album(title, artist, songCount, length, imageURL);
+        albumRespository.save(album);
+        return new RedirectView("/albums");
     }
 
 }
